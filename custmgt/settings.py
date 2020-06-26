716eab5195688cb3ffcffc4a4bf1ccfadc3817cb
asifhaider995 <43353121+asifhaider995@users.noreply.github.com>
2020-06-26 22:33:50 +0600

    Security Fix

--- custmgt/settings.py
+++ custmgt/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT =  587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'asif.haider995@gmail.com'
-EMAIL_HOST_PASSWORD = 'System ##@ macfished'
+EMAIL_HOST_USER = '<your_email>'
+EMAIL_HOST_PASSWORD = '<email_password>'
